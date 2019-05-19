ca88b23c094228ca6a9e082d8c000453cc9c0934
waynewu6250 <waynewu6250@gmail.com>
2019-05-19 23:27:19 +0800

    Get links

---
+++ js/project.js
@@ -0,0 +1,10 @@
+// background
+jQuery(document).ready(function($){
+'use strict';
+    jQuery('body').backstretch([
+      "img/bg/header2.png"
+    , "img/bg/header3.jpg"
+    , "img/bg/header4.jpg"
+    , "img/bg/header5.jpg"
+    ], {duration: 5000, fade: 500});
+});
